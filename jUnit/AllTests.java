c0a4900824aeb2451b515d26410b72f489dce5b5
Dyeh310 <nheyd9292@gmail.com>
2018-01-31 09:29:45 -0500

    Create AllTests.java

---
+++ jUnit/AllTests.java
@@ -0,0 +1,11 @@
+package programmingknowledge;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ addTest.class, ConcatTest.class })
+public class AllTests {
+
+}
