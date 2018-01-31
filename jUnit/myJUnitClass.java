adbbef1988a63e828c66dc4e773f2d5fd4484043
Dyeh310 <nheyd9292@gmail.com>
2018-01-31 09:30:34 -0500

    Create myJUnitClass.java

---
+++ jUnit/myJUnitClass.java
@@ -0,0 +1,13 @@
+package programmingknowledge;
+
+public class myJUnitClass {
+	
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public String ConCat (String a, String b) {
+		return a+b;
+	}
+}
